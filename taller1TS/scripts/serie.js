e07872f2b9503974a38bff7f90ba85bfdddf50b9
MartinUbaque <69693663+MartinUbaque@users.noreply.github.com>
2022-04-19 19:16:08 -0500

    Add files via upload

---
+++ taller1TS/scripts/serie.js
@@ -0,0 +1,13 @@
+var Serie = /** @class */ (function () {
+    function Serie(id, nombre, cadena, temporadas, descripcion, link, imagen) {
+        this.id = id;
+        this.nombre = nombre;
+        this.cadena = cadena;
+        this.temporadas = temporadas;
+        this.descripcion = descripcion;
+        this.link = link;
+        this.imagen = imagen;
+    }
+    return Serie;
+}());
+export { Serie };
